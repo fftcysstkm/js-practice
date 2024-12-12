35c23769e768698ce53ba96ac7a816ee452199ab
Takuma Sato <t.satobm3012@gmail.com>
2024-12-12 22:30:15 +0900

    最初のコミット

---
+++ src/table.js
@@ -0,0 +1,81 @@
+import './style.css'
+
+const modal = document.getElementById('modal')
+const modalDetail = document.getElementById('modal-details')
+
+async function fetchData(params) {
+    try{
+        // 取得
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const users = await response.json()
+        // console.log(users)
+
+        // テーブル生成
+        const tbody = document.getElementById('user-tbody')
+
+        
+        users.forEach(user => {
+
+            const row = document.createElement('tr')
+            row.innerHTML = `
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.phone}</td>
+                <td>${user.email}</td>
+                <td><button class="detail-button">詳細</button</td>
+            `
+            tbody.appendChild(row)
+
+            // 詳細モーダル表示
+            row.querySelector(".detail-button").addEventListener("click", ()=>{
+                modal.style.display = 'flex'
+
+                // 中身表示
+                document.getElementById('user-id').value = user.id
+                document.getElementById('name').value = user.name
+                document.getElementById('phone').value = user.phone
+                document.getElementById('email').value = user.email
+
+                // モーダル内更新ボタン
+                document.getElementById('update-button').addEventListener("click", ()=>{
+                
+                const updatedUser = {
+                    id: document.getElementById('user-id').value,
+                    name: document.getElementById('name').value,
+                    phone: document.getElementById('phone').value,
+                    email: document.getElementById('email').value
+                };
+
+                fetch('http://localhost:8080/users/update', {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(updatedUser)
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                    modal.style.display = 'none';
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+                })
+                
+            })
+
+        });
+
+    }catch(error){
+
+    }
+}
+
+// モーダル閉じる
+document.querySelector('.close-button').addEventListener('click', ()=>{
+    modal.style.display = 'none'
+})
+
+
+window.addEventListener('DOMContentLoaded', fetchData)
