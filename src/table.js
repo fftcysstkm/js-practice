4c142be42bd749a9a0bba1e282cd8f01a12606bb
Takuma Sato <t.satobm3012@gmail.com>
2024-12-14 23:00:09 +0900

    prettier設定

--- src/table.js
+++ src/table.js
@@ -1,83 +1,79 @@
-import './style.css'
+import "./style.css";
 
-const modal = document.getElementById('modal')
-const modalDetail = document.getElementById('modal-details')
+const modal = document.getElementById("modal");
+const modalDetail = document.getElementById("modal-details");
 
 async function fetchData(params) {
-    try{
-        // 取得
-        const response = await fetch('https://jsonplaceholder.typicode.com/users')
-        const users = await response.json()
-        // console.log(users)
+  try {
+    // 取得
+    const response = await fetch("http://localhost:8080/users/list");
+    const users = await response.json();
+    // console.log(users)
 
-        // テーブル生成
-        const tbody = document.getElementById('user-tbody')
+    // テーブル生成
+    const tbody = document.getElementById("user-tbody");
+    tbody.innerHTML = ""; // テーブルの内容をクリア
 
-        
-        users.forEach(user => {
-
-            const row = document.createElement('tr')
-            row.innerHTML = `
+    users.forEach((user) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
                 <td>${user.id}</td>
                 <td>${user.name}</td>
                 <td>${user.phone}</td>
                 <td>${user.email}</td>
                 <td><button class="detail-button">詳細</button</td>
-            `
-            tbody.appendChild(row)
+            `;
+      tbody.appendChild(row);
 
-            // 詳細モーダル表示
-            row.querySelector(".detail-button").addEventListener("click", ()=>{
-                modal.style.display = 'flex'
+      // 詳細モーダル表示
+      row.querySelector(".detail-button").addEventListener("click", () => {
+        modal.style.display = "flex";
 
-                // 中身表示
-                document.getElementById('user-id').value = user.id
-                document.getElementById('name').value = user.name
-                document.getElementById('phone').value = user.phone
-                document.getElementById('email').value = user.email
+        // 中身表示
+        document.getElementById("user-id").value = user.id;
+        document.getElementById("name").value = user.name;
+        document.getElementById("phone").value = user.phone;
+        document.getElementById("email").value = user.email;
 
-                // モーダル内更新ボタン
-                document.getElementById('update-button').addEventListener("click", ()=>{
-                
-                const updatedUser = {
-                    id: document.getElementById('user-id').value,
-                    name: document.getElementById('name').value,
-                    phone: document.getElementById('phone').value,
-                    email: document.getElementById('email').value
-                };
+        // モーダル内更新ボタン
+        document
+          .getElementById("update-button")
+          .addEventListener("click", () => {
+            const updatedUser = {
+              id: document.getElementById("user-id").value,
+              name: document.getElementById("name").value,
+              phone: document.getElementById("phone").value,
+              email: document.getElementById("email").value,
+            };
 
-                fetch(`http://localhost:8080/users/update/${updatedUser.id}`, {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(updatedUser)
-                })
-                .then(response => {
-                    if (response.ok){
-                        modal.style.display = 'none'
-                        return
-                    }
-                    throw new Error('ユーザー更新失敗')
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                });
+            fetch(`http://localhost:8080/users/update/${updatedUser.id}`, {
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(updatedUser),
             })
-                
-            })
-
-        });
-
-    }catch(error){
-
-    }
+              .then((response) => {
+                if (response.ok) {
+                  modal.style.display = "none";
+                  // リロード
+                  fetchData();
+                  return;
+                }
+                throw new Error("ユーザー更新失敗");
+              })
+              .catch((error) => {
+                console.error("Error:", error);
+              });
+          });
+      });
+    });
+  } catch (error) {}
 }
 
 // モーダル閉じる
-document.querySelector('.close-button').addEventListener('click', ()=>{
-    modal.style.display = 'none'
-})
+document.querySelector(".close-button").addEventListener("click", () => {
+  modal.style.display = "none";
+});
 
-
-window.addEventListener('DOMContentLoaded', fetchData)
+window.addEventListener("DOMContentLoaded", fetchData);
