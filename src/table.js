f6a7eb3c594b7203206605789c5f5d0e023e1778
Takuma Sato <t.satobm3012@gmail.com>
2024-12-23 16:30:27 +0900

    リファクタリング1

--- src/table.js
+++ src/table.js
@@ -45,103 +45,111 @@ async function fetchUsers() {
     const URL = "http://localhost:8080/users/list";
     const response = await fetch(URL);
     const users = await response.json();
-    // console.log(users)
-
-    // テーブル生成
-    const tbody = document.getElementById("user-tbody");
-    tbody.innerHTML = ""; // テーブルの内容をクリア
-
-    users.forEach((user) => {
-      const row = document.createElement("tr");
-      row.innerHTML = `
-                <td>${user.id}</td>
-                <td>${user.name}</td>
-                <td>${user.phone}</td>
-                <td>${user.email}</td>
-                <td><button class="detail-button">詳細</button</td>
-            `;
-      tbody.appendChild(row);
-
-      // 詳細モーダル表示
-      row.querySelector(".detail-button").addEventListener("click", () => {
-        modal.style.display = "flex";
-        document.getElementById("register-button").hidden = true;
-        document.getElementById("delete-button").hidden = false;
-        document.getElementById("update-button").hidden = false;
-
-        // 中身表示
-        document.getElementById("user-id").value = user.id;
-        document.getElementById("name").value = user.name;
-        document.getElementById("phone").value = user.phone;
-        document.getElementById("email").value = user.email;
-
-        // モーダル内更新ボタン
-        document
-          .getElementById("update-button")
-          .addEventListener("click", async () => {
-            // バリデーション
-            if (!validateFields()) return;
-
-            const updatedUser = {
-              id: document.getElementById("user-id").value,
-              name: document.getElementById("name").value,
-              phone: document.getElementById("phone").value,
-              email: document.getElementById("email").value,
-            };
-
-            try {
-              const response = await fetch(
-                `http://localhost:8080/users/update/${updatedUser.id}`,
-                {
-                  method: "PUT",
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                  body: JSON.stringify(updatedUser),
-                }
-              );
-
-              if (response.ok) {
-                modal.style.display = "none";
-                // リロード
-                fetchUsers();
-                return;
-              }
-              throw new Error("ユーザー更新失敗");
-            } catch (error) {
-              console.error("Error:", error);
-            }
-          });
-
-        // モーダル内削除ボタン
-        document
-          .getElementById("delete-button")
-          .addEventListener("click", async () => {
-            const deleteId = document.getElementById("user-id").value;
-            try {
-              const response = await fetch(
-                `http://localhost:8080/users/delete/${deleteId}`,
-                {
-                  method: "DELETE",
-                }
-              );
-
-              if (response.ok) {
-                modal.style.display = "none";
-                // リロード
-                fetchUsers();
-                return;
-              }
-              throw new Error("ユーザー削除失敗");
-            } catch (error) {
-              console.error("Error:", error);
-            }
-          });
-      });
-    });
+    renderUserTable(users);
   } catch (error) {
     confirm("ユーザー取得に失敗");
   }
+}
+
+// テーブル描画処理
+function renderUserTable(users) {
+  // テーブル生成
+  const tbody = document.getElementById("user-tbody");
+  tbody.innerHTML = ""; // テーブルの内容をクリア
+
+  users.forEach((user) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+                 <td>${user.id}</td>
+                 <td>${user.name}</td>
+                 <td>${user.phone}</td>
+                 <td>${user.email}</td>
+                 <td><button class="detail-button">詳細</button</td>
+             `;
+    tbody.appendChild(row);
+
+    // 詳細モーダル表示
+    row.querySelector(".detail-button").addEventListener("click", () => {
+      showUserDetails(user);
+    });
+  });
+}
+
+// モーダル表示処理
+function showUserDetails(user) {
+  modal.style.display = "flex";
+  document.getElementById("register-button").hidden = true;
+  document.getElementById("delete-button").hidden = false;
+  document.getElementById("update-button").hidden = false;
+
+  // 中身表示
+  document.getElementById("user-id").value = user.id;
+  document.getElementById("name").value = user.name;
+  document.getElementById("phone").value = user.phone;
+  document.getElementById("email").value = user.email;
+
+  // モーダル内更新ボタン
+  document
+    .getElementById("update-button")
+    .addEventListener("click", async () => {
+      // バリデーション
+      if (!validateFields()) return;
+
+      const updatedUser = {
+        id: document.getElementById("user-id").value,
+        name: document.getElementById("name").value,
+        phone: document.getElementById("phone").value,
+        email: document.getElementById("email").value,
+      };
+
+      try {
+        const response = await fetch(
+          `http://localhost:8080/users/update/${updatedUser.id}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedUser),
+          }
+        );
+
+        if (response.ok) {
+          modal.style.display = "none";
+          // リロード
+          fetchUsers();
+          return;
+        }
+        throw new Error("ユーザー更新失敗");
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    });
+
+  // モーダル内削除ボタン
+  document
+    .getElementById("delete-button")
+    .addEventListener("click", async () => {
+      const deleteId = document.getElementById("user-id").value;
+      try {
+        const response = await fetch(
+          `http://localhost:8080/users/delete/${deleteId}`,
+          {
+            method: "DELETE",
+          }
+        );
+
+        if (response.ok) {
+          modal.style.display = "none";
+          // リロード
+          fetchUsers();
+          return;
+        }
+        throw new Error("ユーザー削除失敗");
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    });
 }
 
 // モーダル閉じる
