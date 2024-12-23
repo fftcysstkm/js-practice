b67a8b13e0febc14b4a25c6c3f6f536c285a71ba
Takuma Sato <t.satobm3012@gmail.com>
2024-12-23 16:14:41 +0900

    ユーザー登録

--- src/table.js
+++ src/table.js
@@ -3,10 +3,47 @@ import "./style.css";
 const modal = document.getElementById("modal");
 const modalDetail = document.getElementById("modal-details");
 
-async function fetchData(params) {
+// 登録モーダル表示、登録処理
+document.getElementById("register-button-top").addEventListener("click", () => {
+  modal.style.display = "flex";
+  const registerButton = document.getElementById("register-button");
+  registerButton.hidden = false;
+  document.getElementById("delete-button").hidden = true;
+  document.getElementById("update-button").hidden = true;
+
+  registerButton.addEventListener("click", async () => {
+    const newUser = {
+      name: document.getElementById("name").value,
+      phone: document.getElementById("phone").value,
+      email: document.getElementById("email").value,
+    };
+
+    try {
+      const response = await fetch("http://localhost:8080/users/create", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newUser),
+      });
+
+      if (response.ok) {
+        modal.style.display = "none";
+        // リロード
+        await fetchUsers();
+        return;
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      confirm("ユーザー登録に失敗");
+    }
+  });
+});
+
+// データ取得処理
+async function fetchUsers() {
   try {
     // 取得
-    const response = await fetch("http://localhost:8080/users/list");
+    const URL = "http://localhost:8080/users/list";
+    const response = await fetch(URL);
     const users = await response.json();
     // console.log(users)
 
@@ -28,6 +65,9 @@ async function fetchData(params) {
       // 詳細モーダル表示
       row.querySelector(".detail-button").addEventListener("click", () => {
         modal.style.display = "flex";
+        document.getElementById("register-button").hidden = true;
+        document.getElementById("delete-button").hidden = false;
+        document.getElementById("update-button").hidden = false;
 
         // 中身表示
         document.getElementById("user-id").value = user.id;
@@ -38,7 +78,10 @@ async function fetchData(params) {
         // モーダル内更新ボタン
         document
           .getElementById("update-button")
-          .addEventListener("click", () => {
+          .addEventListener("click", async () => {
+            // バリデーション
+            if (!validateFields()) return;
+
             const updatedUser = {
               id: document.getElementById("user-id").value,
               name: document.getElementById("name").value,
@@ -46,51 +89,59 @@ async function fetchData(params) {
               email: document.getElementById("email").value,
             };
 
-            fetch(`http://localhost:8080/users/update/${updatedUser.id}`, {
-              method: "PUT",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(updatedUser),
-            })
-              .then((response) => {
-                if (response.ok) {
-                  modal.style.display = "none";
-                  // リロード
-                  fetchData();
-                  return;
+            try {
+              const response = await fetch(
+                `http://localhost:8080/users/update/${updatedUser.id}`,
+                {
+                  method: "PUT",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(updatedUser),
                 }
-                throw new Error("ユーザー更新失敗");
-              })
-              .catch((error) => {
-                console.error("Error:", error);
-              });
+              );
+
+              if (response.ok) {
+                modal.style.display = "none";
+                // リロード
+                fetchUsers();
+                return;
+              }
+              throw new Error("ユーザー更新失敗");
+            } catch (error) {
+              console.error("Error:", error);
+            }
           });
 
         // モーダル内削除ボタン
         document
           .getElementById("delete-button")
-          .addEventListener("click", () => {
+          .addEventListener("click", async () => {
             const deleteId = document.getElementById("user-id").value;
-            fetch(`http://localhost:8080/users/delete/${deleteId}`, {
-              method: "DELETE",
-            })
-              .then((response) => {
-                if (response.ok) {
-                  modal.style.display = "none";
-                  // リロード
-                  fetchData();
-                  return;
+            try {
+              const response = await fetch(
+                `http://localhost:8080/users/delete/${deleteId}`,
+                {
+                  method: "DELETE",
                 }
-                throw new Error("ユーザー削除失敗");
-              })
-              .catch((error) => {
-                console.error("Error:", error);
-              });
+              );
+
+              if (response.ok) {
+                modal.style.display = "none";
+                // リロード
+                fetchUsers();
+                return;
+              }
+              throw new Error("ユーザー削除失敗");
+            } catch (error) {
+              console.error("Error:", error);
+            }
           });
       });
     });
-  } catch (error) {}
+  } catch (error) {
+    confirm("ユーザー取得に失敗");
+  }
 }
 
 // モーダル閉じる
@@ -98,4 +149,17 @@ document.querySelector(".close-button").addEventListener("click", () => {
   modal.style.display = "none";
 });
 
-window.addEventListener("DOMContentLoaded", fetchData);
+// フィールドのバリデーション関数
+function validateFields() {
+  const name = document.getElementById("name").value.trim();
+  const phone = document.getElementById("phone").value.trim();
+  const email = document.getElementById("email").value.trim();
+
+  if (!name || !phone || !email) {
+    alert("すべてのフィールドを入力してください。");
+    return false;
+  }
+  return true;
+}
+
+window.addEventListener("DOMContentLoaded", fetchUsers);
